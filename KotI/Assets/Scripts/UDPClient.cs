60625aec60b4114884a9da9b7e75a50f47f2f57c
Leukino <miquelqg99@gmail.com>
2023-11-23 17:19:24 +0100

    copy pasted code from exercise 2 hehe

--- KotI/Assets/Scripts/UDPClient.cs
+++ KotI/Assets/Scripts/UDPClient.cs
@@ -1,18 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 using UnityEngine;
 
 public class UDPClient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string serverIP = "127.0.0.1"; // Replace with the server's IP address.
+    public int serverPort = 12345; // Specify the port the server is listening on.
+
+    private UdpClient udpClient;
+
+    private void Start()
     {
-        
+        try
+        {
+            // Create a UDP client.
+            udpClient = new UdpClient();
+
+            // Connect to the server with the provided IP and port.
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+            // Send a message to the server.
+            string message = "Hello from the client!";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            udpClient.Send(messageBytes, messageBytes.Length, serverEndPoint);
+
+            Debug.Log("UDP client connected to " + serverIP + ":" + serverPort);
+
+            // Start listening for responses asynchronously.
+            udpClient.BeginReceive(ReceiveCallback, null);
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("Error setting up UDP client: " + e.Message);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReceiveCallback(IAsyncResult ar)
     {
-        
+        try
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, serverPort);
+            byte[] receivedBytes = udpClient.EndReceive(ar, ref serverEndPoint);
+            string responseMessage = Encoding.UTF8.GetString(receivedBytes);
+
+            // Handle the received response from the server.
+            HandleResponse(responseMessage);
+
+            // Continue listening for more responses.
+            udpClient.BeginReceive(ReceiveCallback, null);
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("UDP receive error: " + e.Message);
+        }
+    }
+
+    private void HandleResponse(string responseMessage)
+    {
+        // Handle the received response from the server.
+        Debug.Log("Received response from server: " + responseMessage);
+    }
+
+    private void OnDestroy()
+    {
+        if (udpClient != null)
+        {
+            udpClient.Close();
+        }
+    }
+
+    public void setIpDestination(string newIp)
+    {
+        serverIP = newIp;
     }
 }
--- KotI/Assets/Scripts/UDPServer.cs
+++ KotI/Assets/Scripts/UDPServer.cs
@@ -1,18 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 using UnityEngine;
 
 public class UDPServer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public int port = 12345; // Specify the port to listen on.
+
+    private UdpClient udpListener;
+
+    private void Start()
     {
-        
+        try
+        {
+            // Create a UDP listener on the specified port.
+            udpListener = new UdpClient(port);
+            Debug.Log("UDP server is listening on port " + port);
+
+            // Start listening for incoming messages asynchronously.
+            udpListener.BeginReceive(ReceiveCallback, null);
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("Error setting up UDP server: " + e.Message);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReceiveCallback(IAsyncResult ar)
     {
-        
+        try
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+            byte[] receivedBytes = udpListener.EndReceive(ar, ref remoteEndPoint);
+            string message = Encoding.UTF8.GetString(receivedBytes);
+
+            // Handle the received message, e.g., respond to the client.
+            HandleMessage(message, remoteEndPoint);
+
+            // Continue listening for more messages.
+            udpListener.BeginReceive(ReceiveCallback, null);
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("UDP receive error: " + e.Message);
+        }
+    }
+
+    private void HandleMessage(string message, IPEndPoint remoteEndPoint)
+    {
+        // Handle the received message here. You can send a response back to the client if needed.
+        Debug.Log("Received message from " + remoteEndPoint + ": " + message);
+
+        // Example: Sending a response to the client.
+        string responseMessage = "Hello from the server!";
+        byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+        udpListener.Send(responseBytes, responseBytes.Length, remoteEndPoint);
+    }
+
+    private void OnDestroy()
+    {
+        if (udpListener != null)
+        {
+            udpListener.Close();
+        }
     }
 }
