6fbf028281ea84fddbf3719db6d7f3c3b75222a3
Leukino <miquelqg99@gmail.com>
2023-11-24 20:00:52 +0100

    packet management, player movement (kinda messi but works)
    Co-Authored-By: Okis. <47740554+oscarta3@users.noreply.github.com>

--- KotI/Assets/Scripts/UDPClient.cs
+++ KotI/Assets/Scripts/UDPClient.cs
@@ -5,28 +5,38 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using UnityEngine;
+using UnityEditor.VersionControl;
 
 public class UDPClient : MonoBehaviour
 {
     public string serverIP = "127.0.0.1"; // Replace with the server's IP address.
     public int serverPort = 12345; // Specify the port the server is listening on.
 
-    private UdpClient udpClient;
-
+    public UdpClient udpClient;
+    public IPEndPoint serverEndPoint;
+    public GameObject clientPlayer;
     private void Start()
     {
+
         try
         {
             // Create a UDP client.
             udpClient = new UdpClient();
 
             // Connect to the server with the provided IP and port.
-            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
 
             // Send a message to the server.
             string message = "Hello from the client!";
-            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            //byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            Packet pack = new Packet("Manolo", Status.Connect, new Vector3(0, 1, 0), message);
+
+
+            byte[] messageBytes = SerializePacket(pack);  //Encoding.UTF8.GetBytes(responseMessage);
+
             udpClient.Send(messageBytes, messageBytes.Length, serverEndPoint);
+
+
 
             Debug.Log("UDP client connected to " + serverIP + ":" + serverPort);
 
@@ -39,16 +49,36 @@ public class UDPClient : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) 
+        {
+        }
+            Packet pack = new Packet("Manolo", Status.Movement, clientPlayer.transform.position, "I have moved");
+            byte[] messageBytes = SerializePacket(pack);  //Encoding.UTF8.GetBytes(responseMessage);
+            udpClient.Send(messageBytes, messageBytes.Length, serverEndPoint);
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+
+        //    Packet pack = new Packet("Manolo", Status.Chat, GameObject.Find("Player").transform.position, "I have chatted");
+        //    byte[] messageBytes = SerializePacket(pack);  //Encoding.UTF8.GetBytes(responseMessage);
+
+        //    udpClient.Send(messageBytes, messageBytes.Length, serverEndPoint);
+        //}
+
+
+    }
+
     private void ReceiveCallback(IAsyncResult ar)
     {
         try
         {
             IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, serverPort);
             byte[] receivedBytes = udpClient.EndReceive(ar, ref serverEndPoint);
-            string responseMessage = Encoding.UTF8.GetString(receivedBytes);
-
+            //string responseMessage = Encoding.UTF8.GetString(receivedBytes);
+            Packet responsePacket = DeserializePacket(receivedBytes);
             // Handle the received response from the server.
-            HandleResponse(responseMessage);
+            HandleResponse(responsePacket);
 
             // Continue listening for more responses.
             udpClient.BeginReceive(ReceiveCallback, null);
@@ -59,10 +89,10 @@ public class UDPClient : MonoBehaviour
         }
     }
 
-    private void HandleResponse(string responseMessage)
+    private void HandleResponse(Packet responsePacket)
     {
         // Handle the received response from the server.
-        Debug.Log("Received response from server: " + responseMessage);
+        Debug.Log("Received response from server: " + responsePacket.message);
     }
 
     private void OnDestroy()
@@ -77,4 +107,40 @@ public class UDPClient : MonoBehaviour
     {
         serverIP = newIp;
     }
+
+    public class Packet
+    {
+        public string user;
+        public Status status;
+        public Vector3 position;
+        public string message;
+        public Packet(String user, Status status, Vector3 position, string message)
+        {
+            this.user = user;
+            this.status = status;
+            this.position = position;
+            this.message = message;
+        }
+    }
+    public enum Status
+    {
+        Connect, 
+        Disconnect,  
+        Chat,
+        Movement,
+        Idle
+    }
+
+    public byte[] SerializePacket(Packet toSend)
+    {
+        string json = JsonUtility.ToJson(toSend);
+        return System.Text.Encoding.UTF8.GetBytes(json);
+    }
+    public Packet DeserializePacket(byte[] toReceive)
+    {
+        string json = System.Text.Encoding.UTF8.GetString(toReceive);
+        return JsonUtility.FromJson<Packet>(json);
+    }
+
+
 }
--- KotI/Assets/Scripts/UDPServer.cs
+++ KotI/Assets/Scripts/UDPServer.cs
@@ -5,21 +5,54 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using UnityEngine;
+using UnityEditor.VersionControl;
 
 public class UDPServer : MonoBehaviour
 {
     // Start is called before the first frame update
     public int port = 12345; // Specify the port to listen on.
 
-    private UdpClient udpListener;
+    public UdpClient udpListener;
+    public GameObject playerPrefab;
+    public class Player
+    {
+        public string userID;
+        public GameObject userGO;
+        public Vector3 position;
+        public Player(string userID, GameObject userGO, Vector3 position)
+        {
+            this.userID = userID;
+            this.userGO = userGO;
+            this.position = position;
+        }
+    }
 
+    public List<Player> playersOnline = new List<Player>();
+    public GameObject playerFromClient;
+    public Packet lastPacket;
     private void Start()
     {
+        playerFromClient = GameObject.Find("PlayerFromClient");
         try
         {
             // Create a UDP listener on the specified port.
             udpListener = new UdpClient(port);
             Debug.Log("UDP server is listening on port " + port);
+
+            // Start listening for incoming messages asynchronously.
+            //udpListener.BeginReceive(ReceiveCallback, null);
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError("Error setting up UDP server: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        try
+        {
+            // Create a UDP listener on the specified port.
 
             // Start listening for incoming messages asynchronously.
             udpListener.BeginReceive(ReceiveCallback, null);
@@ -28,36 +61,77 @@ public class UDPServer : MonoBehaviour
         {
             Debug.LogError("Error setting up UDP server: " + e.Message);
         }
+
+        if (lastPacket!= null) 
+        {
+            if (lastPacket.status == Status.Connect)
+            {
+                Player newPlayer = new Player(lastPacket.user, GameObject.Find("PlayerFromClient"), new Vector3(0, 0, 0));
+                playersOnline.Add(newPlayer);
+            }
+            //if (lastPacket.status == Status.Chat)
+                //TODO when we received some chat hehe
+            if (lastPacket.status == Status.Movement)
+            {
+                
+                playerFromClient.transform.position = new Vector3(lastPacket.position.x, lastPacket.position.y+5, lastPacket.position.z);
+                //TODO update Players List
+            }
+            lastPacket = null;
+        }
+        
     }
 
-    private void ReceiveCallback(IAsyncResult ar)
+    public void ReceiveCallback(IAsyncResult ar)
     {
+
         try
         {
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
             byte[] receivedBytes = udpListener.EndReceive(ar, ref remoteEndPoint);
-            string message = Encoding.UTF8.GetString(receivedBytes);
+            //string message = Encoding.UTF8.GetString(receivedBytes);
+            Packet message = DeserializePacket(receivedBytes);
 
             // Handle the received message, e.g., respond to the client.
             HandleMessage(message, remoteEndPoint);
 
             // Continue listening for more messages.
-            udpListener.BeginReceive(ReceiveCallback, null);
+            //udpListener.BeginReceive(ReceiveCallback, null);
         }
         catch (SocketException e)
         {
             Debug.LogError("UDP receive error: " + e.Message);
         }
+
+
     }
 
-    private void HandleMessage(string message, IPEndPoint remoteEndPoint)
+    public void HandleMessage(Packet message, IPEndPoint remoteEndPoint)
     {
         // Handle the received message here. You can send a response back to the client if needed.
-        Debug.Log("Received message from " + remoteEndPoint + ": " + message);
+        Debug.Log("Received message from " + remoteEndPoint + ": " + message.message);
 
+        lastPacket = message;
+
+        string messageToSend = "";
+
+        //if message.status is Connect means it's the first time
+        if (message.status == Status.Connect)
+        {
+            messageToSend = "Welcome, " + message.user;
+        }
+        if (message.status == Status.Chat)
+            messageToSend = message.user + " chatted: " + message.message;
+
+
+        messageToSend = "Moving user " + message.user + " to " + message.position;
         // Example: Sending a response to the client.
-        string responseMessage = "Hello from the server!";
-        byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+        //string responseMessage = "Hello from the server!";
+
+        Packet pack = new Packet("Server", Status.Connect, new Vector3(0, 0, 0), messageToSend);
+
+
+        byte[] responseBytes = SerializePacket(pack);  //Encoding.UTF8.GetBytes(responseMessage);
         udpListener.Send(responseBytes, responseBytes.Length, remoteEndPoint);
     }
 
@@ -68,4 +142,40 @@ public class UDPServer : MonoBehaviour
             udpListener.Close();
         }
     }
+
+
+    public class Packet
+    {
+        public string user;
+        public Status status;
+        public Vector3 position;
+        public string message;
+        public Packet(String user, Status status, Vector3 position, string message)
+        {
+            this.user = user;
+            this.status = status;
+            this.position = position;
+            this.message = message;
+        }
+    }
+    public enum Status
+    {
+        Connect, 
+        Disconnect, 
+        Chat,
+        Movement,
+        Idle
+    }
+
+    public byte[] SerializePacket(Packet toSend)
+    {
+        string json = JsonUtility.ToJson(toSend);
+        return System.Text.Encoding.UTF8.GetBytes(json);
+    }
+    public Packet DeserializePacket(byte[] toReceive)
+    {
+        string json = System.Text.Encoding.UTF8.GetString(toReceive);
+        return JsonUtility.FromJson<Packet>(json);
+    }
+
 }
