c9b7f2cb765fb879c72ede5303f417eb8f18b8f0
oscarta3 <47740554+oscarta3@users.noreply.github.com>
2024-01-25 00:54:55 +0100

    name template for leukino jeje! (farming commits)

--- KotI/Assets/Scripts/PlayerOverheadDisplay.cs
+++ KotI/Assets/Scripts/PlayerOverheadDisplay.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-namespace KotI.PlayerNames
-{
-    public class PlayersOverheadDisplay
+    public class PlayersOverheadDisplay : MonoBehaviour
     {
         [SerializeField] private TMP_Text displayNameText;
+
+        public void Awake()
+        {
+            displayNameText.text = gameObject.name;
+        }
     }
-}
