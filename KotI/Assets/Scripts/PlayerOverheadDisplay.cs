e33aaea493a53113af0f42b0db36205e32d7ab77
carlosarnau <arnau.carlos01@gmail.com>
2024-01-24 14:12:48 +0100

    Adding ready button and TextMeshPro for player name

---
+++ KotI/Assets/Scripts/PlayerOverheadDisplay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace KotI.PlayerNames
+{
+    public class PlayersOverheadDisplay
+    {
+        [SerializeField] private TMP_Text displayNameText;
+    }
+}
